8d4bffb4e98b71bba6b3bb78d487a44b5aa9c80f
Yu Liang <yu.liang@thekono.com>
2017-05-06 22:28:49 +0800

    Modify setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,30 @@
 from os.path import exists
 from setuptools import setup, find_packages
 
+install_requires = []
+
+if exists('requirements.txt'):
+    install_requires = open('requirements.txt').read().strip().split('\n')
+
 setup(
     name='thumbor_rekognition',
     version=open('VERSION').read().strip(),
     # Your name & email here
-    author='',
-    author_email='',
+    author='Yu Liang',
+    author_email='yu.liang@thekono.com',
     # If you had thumbor_rekognition.tests, you would also include that in this list
     packages=find_packages(),
     # Any executable scripts, typically in 'bin'. E.g 'bin/do-something.py'
     scripts=[],
     # REQUIRED: Your project's URL
-    url='',
+    url='https://github.com/yu-liang-kono/thumbor_rekognition',
     # Put your license here. See LICENSE.txt for more information
-    license='',
+    license=open('LICENSE').read() if exists('LICENSE') else '',
     # Put a nice one-liner description here
-    description='',
+    description='Enable thumbor to use AWS rekognition to run face detection',
     long_description=open('README.rst').read() if exists("README.rst") else "",
     # Any requirements here, e.g. "Django >= 1.1.1"
-    install_requires=[
-        
-    ],
+    install_requires=install_requires,
     # Ensure we include files from the manifest
-    include_package_data=True,
+    include_package_data=True
 )
